bcc41d4b984d522817121594ec6d99ccdee672dd
yahortsaryk <egortsaryk@gmail.com>
2025-05-19 17:10:04 +0200

    fix: deprecating functions related to total usage

--- src/traits/node.rs
+++ src/traits/node.rs
@@ -5,13 +5,13 @@ use sp_runtime::DispatchError;
 use crate::{ClusterId, NodeParams, NodePubKey};
 
 pub trait NodeManager<AccountId> {
-    fn get_cluster_id(node_pub_key: &NodePubKey) -> Result<Option<ClusterId>, DispatchError>;
-    fn exists(node_pub_key: &NodePubKey) -> bool;
-    fn get_node_provider_id(node_pub_key: &NodePubKey) -> Result<AccountId, DispatchError>;
-    fn get_node_params(node_pub_key: &NodePubKey) -> Result<NodeParams, DispatchError>;
+    fn get_cluster_id(node_key: &NodePubKey) -> Result<Option<ClusterId>, DispatchError>;
+    fn exists(node_key: &NodePubKey) -> bool;
+    fn get_node_provider_id(node_key: &NodePubKey) -> Result<AccountId, DispatchError>;
+    fn get_node_params(node_key: &NodePubKey) -> Result<NodeParams, DispatchError>;
     #[cfg(feature = "runtime-benchmarks")]
     fn create_node(
-        node_pub_key: NodePubKey,
+        node_key: NodePubKey,
         provider_id: AccountId,
         node_params: NodeParams,
     ) -> DispatchResult;
